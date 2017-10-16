da355c226609d23bf78a499ba7ca4c02eea283bf
karuru6225 <e2hjhi@gmail.com>
2017-10-16 16:31:10 +0900

    完成版

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,4 @@
+/** 全体の設定 **/
 var AppPot = AppPotSDK.getService({
   url: 'https://trial.apppot.net/apppot/',
   appId: 'xxxx',
@@ -8,54 +9,48 @@ var AppPot = AppPotSDK.getService({
 
 var navi;
 ons.ready(function() {
-    navi = document.getElementById('navi');
+  navi = document.getElementById('navi');
+  document.addEventListener('show', function(event) {
+    switch(event.target.id){
+      case 'list-page':
+        getCustomerList();
+        break;
+    }
+  });
 });
 
+
+/** ログイン画面 **/
 function login() {
   var userName = document.getElementById('userName').value;
   var password = document.getElementById('password').value;
-  
+
+  /******** Exercise 3 : ログイン機能の実装 ********/
   AppPot.LocalAuthenticator.login(userName, password)
   .then(() => {
-      console.log("Logined.");
-      return navi.pushPage('list.html');
+    console.log("ログインしました。");
+    navi.pushPage('list.html');
   })
-  .then(getCustomerList)
   .catch((error) => {
-      console.log("Login failed.");
-      alert('ログインに失敗しました');
+    console.log("ログインに失敗しました。");
+    alert('ログインに失敗しました');
   });
+  /******** Exercise 3 : ここまで ********/
 }
 
+
+/** 取引先一覧画面 **/
 function getCustomerList() {
   AppPot.Gateway.get("customer-db", "CustomerCompany", null, null, null)
   .then((response) => {
-      console.log(JSON.stringify(response));
-      var list = document.getElementById('customerList');
-      list.innerHTML = '';
-      var customerData = response.CustomerCompany || [];
-      customerData.forEach((customer, index) => {
-          var template = '<ons-list-item modifier="chevron" onclick="showCustomerData(' + customer.companyId + ')">' + customer.companyName + '</ons-list-item>';
-          ons.createElement(template, { append: list });
-      }); 
-  });
-}
-
-function showCustomerData(companyId) {
-  var page;
-  navi.pushPage('detail.html').then((_page) => {
-      page = _page;
-      return AppPot.Gateway.get("customer-db", "query", {
-          query: "select * from CustomerCompany where companyId = '" + companyId + "'"
-      }, null);
-  })
-  .then((response) => {
-      var customer = response.query[0];
-      page.querySelector('[name="companyName"]>div').textContent = customer.companyName;
-      page.querySelector('[name="companyNameKana"]>div').textContent = customer.companyNameKana;
-      page.querySelector('[name="zipCode"]>div').textContent = customer.zipCode;
-      page.querySelector('[name="address"]>div').textContent = customer.address;
-      page.querySelector('[name="phoneNumber"]>div').innerHTML = '<a href="tel:' + customer.phoneNumber + '">' + customer.phoneNumber + '</a>';
+    console.log(JSON.stringify(response));
+    var list = document.getElementById('customerList');
+    list.innerHTML = '';
+    var customerData = response.CustomerCompany || [];
+    customerData.forEach((customer, index) => {
+      var template = '<ons-list-item modifier="chevron" onclick="showCustomerData(' + customer.companyId + ')">' + customer.companyName + '</ons-list-item>';
+      ons.createElement(template, { append: list });
+    });
   });
 }
 
@@ -63,19 +58,51 @@ function addCustomerData() {
   navi.pushPage('regist.html');
 }
 
+
+/** 取引先詳細画面 **/
+function showCustomerData(companyId) {
+  var page;
+  navi.pushPage('detail.html').then((_page) => {
+    page = _page;
+    /******** Exercise 4 : 既存データベースへのSELECT機能の実装 ********/
+    return AppPot.Gateway.get("customer-db", "query", {
+        query: "select * from CustomerCompany where companyId = '" + companyId + "'"
+    }, null);
+  })
+  .then((response) => {
+    var customer = response.query[0];
+    renderCustomerData(page, customer);
+    /******** Exercise 4 : ここまで ********/
+  });
+}
+
+function renderCustomerData(page, customer) {
+  page.querySelector('[name="companyName"]>div').textContent = customer.companyName;
+  page.querySelector('[name="companyNameKana"]>div').textContent = customer.companyNameKana;
+  page.querySelector('[name="zipCode"]>div').textContent = customer.zipCode;
+  page.querySelector('[name="address"]>div').textContent = customer.address;
+  page.querySelector('[name="phoneNumber"]>div').innerHTML = '<a href="tel:' + customer.phoneNumber + '">' + customer.phoneNumber + '</a>';
+}
+
+
+/** 取引先登録画面 **/
 function regist() {
-  var requestJson = {
+  var requestJson = getSendData();
+  /******** Exercise 5 : 既存データベースへの登録機能の実装 ********/
+  AppPot.Gateway.post("customer-db", "CustomerCompany", null, requestJson, null)
+  .then((response) => {
+    alert("登録しました");
+    return navi.popPage();
+  });
+  /******** Exercise 5 : ここまで ********/
+}
+
+function getSendData() {
+  return {
     "companyName" : document.getElementById('companyName').value,
     "companyNameKana" : document.getElementById('companyNameKana').value,
     "zipCode" : document.getElementById('zipCode').value,
     "address" : document.getElementById('address').value,
     "phoneNumber" : document.getElementById('phoneNumber').value
-  }
-  AppPot.Gateway.post("customer-db", "CustomerCompany", null, requestJson, null)
-  .then((response) => {
-      console.log(response);
-      alert("登録しました");
-      return navi.popPage();
-  })
-  .then(getCustomerList)
+  };
 }
